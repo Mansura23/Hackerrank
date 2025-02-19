c2a0371b3c2aefc6eea857d7944cf2d3a48fd520
Mansura <badalovamansura40@gmail.com>
2025-02-19 15:56:12 +0400

    project attached

--- src/main/java/Hackerrank/problems/Basic/Introduction/JavaStdinStdout.java
+++ src/main/java/Hackerrank/problems/Basic/Introduction/JavaStdinStdout.java
@@ -6,10 +6,10 @@ public class JavaStdinStdout {
     public static void main(String[] args) {
 
 
-        Scanner scanner=new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
         String myString = scanner.next();
         int myInt = scanner.nextInt();
-        int mysecondInt=scanner.nextInt();
+        int mysecondInt = scanner.nextInt();
         scanner.close();
         System.out.println(myString);
         System.out.println(myInt);
