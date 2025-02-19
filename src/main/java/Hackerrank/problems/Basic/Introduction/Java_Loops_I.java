e690c6bd758ca2534d3faecf039a5ae966cc7377
Mansura <badalovamansura40@gmail.com>
2025-02-19 22:17:53 +0400

    project attached

--- src/main/java/Hackerrank/problems/Basic/Introduction/Java_Loops_I.java
+++ src/main/java/Hackerrank/problems/Basic/Introduction/Java_Loops_I.java
@@ -1,5 +1,19 @@
 package Hackerrank.problems.Basic.Introduction;
 
+import java.util.Scanner;
+
 public class Java_Loops_I {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int N = scanner.nextInt();
+
+
+        for (int i = 1; i <= 10; i++) {
+            int product = i * N;
+            System.out.println(N + " x " + i + " = " + product);
+
+        }
+    }
 
 }
