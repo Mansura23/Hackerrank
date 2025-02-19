fede48802e47a8120f866aeca21f78f5e69d79c9
Mansura <badalovamansura40@gmail.com>
2025-02-19 15:55:32 +0400

    project attached

---
+++ src/main/java/Hackerrank/problems/Basic/Introduction/JavaStdinStdout.java
@@ -0,0 +1,18 @@
+package Hackerrank.problems.Basic.Introduction;
+
+import java.util.Scanner;
+
+public class JavaStdinStdout {
+    public static void main(String[] args) {
+
+
+        Scanner scanner=new Scanner(System.in);
+        String myString = scanner.next();
+        int myInt = scanner.nextInt();
+        int mysecondInt=scanner.nextInt();
+        scanner.close();
+        System.out.println(myString);
+        System.out.println(myInt);
+        System.out.println(mysecondInt);
+    }
+}
