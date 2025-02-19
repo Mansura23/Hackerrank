9f253c83afa3d14a1e1d303ac1c8fa91c4f72c93
Mansura <badalovamansura40@gmail.com>
2025-02-19 16:02:17 +0400

    project attached

--- src/main/java/Hackerrank/problems/Basic/Introduction/Java_If_Else.java
+++ src/main/java/Hackerrank/problems/Basic/Introduction/Java_If_Else.java
@@ -3,27 +3,21 @@ package Hackerrank.problems.Basic.Introduction;
 import java.util.Scanner;
 
 public class Java_If_Else {
-    public static void main(String[] args)  {
-        Scanner scanner=new Scanner(System.in);
-        int N=scanner.nextInt();
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int N = scanner.nextInt();
 
 
-
-
-
-        if(N%2==1){
+        if (N % 2 == 1) {
             System.out.println("Weird");
 
-        }
-        else if(N%2==0 & N>2 & N<5){
+        } else if (N % 2 == 0 & N > 2 & N < 5) {
             System.out.println("Not Weird");
 
-        }
-        else if(N%2==0 & N>6 & N<=20){
+        } else if (N % 2 == 0 & N > 6 & N <= 20) {
             System.out.println("Weird");
 
-        }
-        else{
+        } else {
             System.out.println("Not Weird");
         }
     }
