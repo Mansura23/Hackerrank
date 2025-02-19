4df99e90dc2de79fa1202654234ca10a54d5717b
Mansura <badalovamansura40@gmail.com>
2025-02-19 22:23:50 +0400

    project attached

---
+++ src/main/java/Hackerrank/problems/Basic/Introduction/JavaDatatypes.java
@@ -0,0 +1,37 @@
+package Hackerrank.problems.Basic.Introduction;
+
+import java.util.Scanner;
+
+public class JavaDatatypes {
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner scanner=new Scanner(System.in);
+
+        int count=scanner.nextInt();
+
+        for(int i=0; i<count; i++){
+            try{
+
+                long num=scanner.nextLong();
+                System.out.println(num + " can be fitted in: ");
+                if(num>=Byte.MIN_VALUE & num<=Byte.MAX_VALUE) System.out.println("* byte");
+                if(num>=Short.MIN_VALUE & num<=Short.MAX_VALUE)System.out.println("* short");
+                if(num>=Integer.MIN_VALUE & num<=Integer.MAX_VALUE)System.out.println("* int");
+                if(num>=Long.MIN_VALUE & num<=Long.MAX_VALUE)System.out.println("* long");
+
+
+
+
+
+            }
+            catch(Exception e){
+                System.out.println(scanner.next()+" can't be fitted anywhere.");
+            }
+
+
+
+        }
+
+
+    }
+}
