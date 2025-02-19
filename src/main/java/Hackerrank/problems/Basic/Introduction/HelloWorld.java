1f51e50af81141fc89216b2eedbb999469ba6ff3
Mansura <badalovamansura40@gmail.com>
2025-02-19 15:49:10 +0400

    project attached

---
+++ src/main/java/Hackerrank/problems/Basic/Introduction/HelloWorld.java
@@ -0,0 +1,9 @@
+package Hackerrank.problems.Basic.Introduction;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+
+        System.out.println("Hello, World.");
+        System.out.println("Hello, Java.");
+    }
+}
