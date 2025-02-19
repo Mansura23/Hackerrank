5b7e90a85470d8cada5a55ca654b6ba4e28b96e1
Mansura <badalovamansura40@gmail.com>
2025-02-19 22:13:43 +0400

    project attached

---
+++ src/main/java/Hackerrank/problems/Basic/Introduction/Java_Output_Formatting.java
@@ -0,0 +1,26 @@
+package Hackerrank.problems.Basic.Introduction;
+
+import java.util.Scanner;
+
+public class Java_Output_Formatting {
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner scanner=new Scanner(System.in);
+
+
+        System.out.println("================================");
+        for(int i=0;i<3;i++){
+            String str1=scanner.next();
+            int num1=scanner.nextInt();
+            System.out.printf("%-15s%03d\n",  str1, num1);
+        }
+
+
+
+
+
+
+        System.out.println("================================");
+
+    }
+}
