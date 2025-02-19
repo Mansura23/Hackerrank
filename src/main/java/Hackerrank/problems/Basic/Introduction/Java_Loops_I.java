ac24860970d52f47c20eaf3e3890fbaf153bf18e
Mansura <badalovamansura40@gmail.com>
2025-02-19 22:15:31 +0400

    project attached

---
+++ src/main/java/Hackerrank/problems/Basic/Introduction/Java_Loops_I.java
@@ -0,0 +1,5 @@
+package Hackerrank.problems.Basic.Introduction;
+
+public class Java_Loops_I {
+
+}
--- src/main/java/Hackerrank/problems/Basic/Introduction/Java_Output_Formatting.java
+++ src/main/java/Hackerrank/problems/Basic/Introduction/Java_Output_Formatting.java
@@ -5,19 +5,15 @@ import java.util.Scanner;
 public class Java_Output_Formatting {
     public static void main(String[] args) {
         /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
-        Scanner scanner=new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
 
 
         System.out.println("================================");
-        for(int i=0;i<3;i++){
-            String str1=scanner.next();
-            int num1=scanner.nextInt();
-            System.out.printf("%-15s%03d\n",  str1, num1);
+        for (int i = 0; i < 3; i++) {
+            String str1 = scanner.next();
+            int num1 = scanner.nextInt();
+            System.out.printf("%-15s%03d\n", str1, num1);
         }
-
-
-
-
 
 
         System.out.println("================================");
